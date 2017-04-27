ec2c90ea599c2fec0dfd1bc0dc1f6fa8ff96ed68
derushio <narumi18wa@gmail.com>
2017-04-27 20:50:07 +0900

    コンパイラ改善

--- tool/build/build_babel.sh
+++ tool/build/build_babel.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-find ./dist/script -type d -name lib -prune -o -type f -name *.js -print | xargs browserify -o dist/script/bundle.js -t [ babelify --presets [ es2015 ] ]
+find ./dist/script/tag -type f -name *.js -print | xargs browserify -o dist/script/bundle.js -t [ babelify --presets [ es2015 ] ]
