f1f31b684befeaabd30dd9881ed2931143aaccc6
derushio <narumi18wa@gmail.com>
2017-04-28 22:01:41 +0900

    インストールスクリプトの誤りを修正

--- install-npm-script.js
+++ install-npm-script.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
-json["build"] = "node tool/build/build.js";
-json["build-babel"] = "tool/build/build_babel.sh";
+json["scripts"]["build"] = "node tool/build/build.js";
+json["scripts"]["build-babel"] = "tool/build/build_babel.sh";
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
