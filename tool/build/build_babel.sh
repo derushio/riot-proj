28be2de373763a2bc854ddb49907150dc3e759c9
derushio <narumi18wa@gmail.com>
2017-05-16 16:35:51 +0900

    ビルドスクリプト改善

--- tool/build/build_babel.sh
+++ tool/build/build_babel.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-find ./dist/script/* -type d -name lib -prune -o -type d -name entry -prune -o -type f -name *.js -print | xargs browserify -o dist/script/bundle.js -t [ babelify --presets [ es2015 ] ]
-find ./dist/script/* -type f -name bundle.js -prune -o -type d -name lib -prune -o -type d -name entry -prune -o -type d -print | xargs rm -rf
+find ./dist/script/* \( -name lib -o -name entry \) -prune -o -type f -name "*.js" -print | xargs browserify -o dist/script/bundle.js -t [ babelify --presets [ es2015 ] ]
+find ./dist/script/* \( -name lib -o -name entry -o -name bundle.js \) -prune -o -type f -print | xargs rm -f
+find ./dist/script/* \( -name lib -o -name entry -o -name bundle.js \) -prune -o -type d -print | xargs rm -rf
